f98a29bfb00ef97b7f3b09a6a2784b1c4aaa54f1
Faisal-pixel <adamsfaisal2003@gmail.com>
2025-02-02 00:27:49 +0100

    updated the cors origin

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }))
